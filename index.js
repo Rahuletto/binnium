1c1a588bfb1378177c5d03a1ca1fe1f6354f597f
Critical Carpet <criticalcarpet.dev@gmail.com>
2022-07-04 14:25:34 +0000

    feat(linter): add and configure eslint

--- index.js
+++ index.js
@@ -2,38 +2,38 @@
 // This is a fun project made under 2 days ;)
 // ------------------------------------------
 
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const express = require("express");
-const app = express();
-import { nanoid } from "nanoid";
-const bodyParser = require("body-parser");
-require("dotenv").config();
-const ejs = require("ejs");
+import { createRequire } from 'module'
+import { nanoid } from 'nanoid'
+import { Model } from './model.js'
+const require = createRequire(import.meta.url)
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const ejs = require('ejs')
 
-const path = require("path");
+const path = require('path')
 
 // Mongoose
-const mongoose = require("mongoose");
-import { model } from "./model.js";
+const mongoose = require('mongoose')
 
 mongoose
   .connect(process.env.mongo)
   .catch((err) => {
-    console.log("Error connecting to mongodb");
-    console.log(err);
-    process.exit(1);
+    console.log('Error connecting to mongodb')
+    console.log(err)
+    process.exit(1)
   })
-  .then(() => console.log("Connected to mongodb"));
+  .then(() => console.log('Connected to mongodb'))
 
 // Using bodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Set engine to EJS
-app.engine("html", ejs.renderFile);
-app.set("view engine", "html");
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
 
-app.use(express.static("./site"));
+app.use(express.static('./site'))
 
 // ----------------------------
 // All the Functions necessary
@@ -42,65 +42,63 @@ app.use(express.static("./site"));
 // A Simple function to render files
 const renderTemplate = (res, req, template, data = {}) => {
   const baseData = {
-    path: req.path,
-  };
+    path: req.path
+  }
   res.render(
     path.resolve(`${process.cwd()}${path.sep}site/${template}`),
     Object.assign(baseData, data)
-  );
-};
+  )
+}
 
 // A length of 12 makes takes 1 thousand years to have a 1% probability of collision at a rate of 1000 ids per hour
-const makeid = (length) => (length ? nanoid(length) : nanoid(12));
+const makeid = (length) => (length ? nanoid(length) : nanoid(12))
 
 // ----------------------------
 
 // Home Page. You can set it into create.ejs
-app.get("/", (req, res) => {
-  renderTemplate(res, req, "index.ejs");
-});
+app.get('/', (req, res) => {
+  renderTemplate(res, req, 'index.ejs')
+})
 
-app.get("/create", async (req, res) => {
-  renderTemplate(res, req, "create.ejs");
-});
+app.get('/create', async (req, res) => {
+  renderTemplate(res, req, 'create.ejs')
+})
 
 // POST request from the form to create bins/paste
-app.post("/create", async (req, res) => {
-  if (req.body.code == "" || !req.body.code) {
-    return res.redirect("/create");
+app.post('/create', async (req, res) => {
+  if (req.body.code === '' || !req.body.code) {
+    return res.redirect('/create')
   }
 
-  const id = makeid(12); // Create a special UID (8 characters)
+  const id = makeid(12) // Create a special UID (8 characters)
 
-  const obj = new model({
-    title: req.body.title || "Untitled",
-    desc: req.body.desc || "No Description",
+  const obj = new Model({
+    title: req.body.title || 'Untitled',
+    desc: req.body.desc || 'No Description',
     code: req.body.code
-      .replaceAll("'", `\'`)
-      .replaceAll('"', `\"`)
-      .replaceAll("`", `\``), // Escape some characters
+      .replaceAll("'", '\'')
+      .replaceAll('"', '"')
+      .replaceAll('`', '`'), // Escape some characters
     uid: id,
     language: req.body.language,
-    filename: req.body.filename,
-  });
+    filename: req.body.filename
+  })
 
-  const result = await obj.save();
+  await obj.save()
 
-  res.redirect(`/file/${id}`);
-});
+  res.redirect(`/file/${id}`)
+})
 
-app.get("/file/:uid", async (req, res) => {
-  let data = await model.findOne({ uid: req.params.uid });
+app.get('/file/:uid', async (req, res) => {
+  const data = await Model.findOne({ uid: req.params.uid })
 
-  console.log(data);
-
-  if (!data) return renderTemplate(res, req, "error.ejs");
+  if (!data) return renderTemplate(res, req, 'error.ejs')
   // Throw error if the file is not found.
-  else renderTemplate(res, req, "file.ejs", { data: data });
-});
+  else renderTemplate(res, req, 'file.ejs', { data })
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Listening to PORT: ${port}`);
-});
+  console.log(`Listening to PORT: ${port}`)
+})
--- model.js
+++ model.js
@@ -1,14 +1,14 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-let mongoose = require("mongoose");
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const mongoose = require('mongoose')
 
-let models = new mongoose.Schema({
-  uid: "string",
-  title: "string",
-  desc: "string",
-  code: "string",
-  language: "string",
-  filename: "string",
-});
+const models = new mongoose.Schema({
+  uid: 'string',
+  title: 'string',
+  desc: 'string',
+  code: 'string',
+  language: 'string',
+  filename: 'string'
+})
 
-export const model = mongoose.model("binnium", models);
+export const Model = mongoose.model('binnium', models)
